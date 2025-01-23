b19e9d8a05a41c840ee1a96469e1758f7b8240b3
kaavyaMagesh <kaavyamagesh1609@gmail.com>
2025-01-23 08:21:07 +0530

    Time: 3 ms (13.79%) | Memory: 9.4 MB (24.84%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.c
+++ 605-can-place-flowers/can-place-flowers.c
@@ -1,11 +1,23 @@
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
-      for (int i = 0; i < flowerbedSize && n > 0; i++) {
-        bool prev = i == 0 || !flowerbed[i - 1];
-        bool fut = i == flowerbedSize - 1 || !flowerbed[i + 1];
-        if (prev && fut && !flowerbed[i]) {
-            flowerbed[i] = 1;
-            n--;
-        }
+    if(n==0) return true;
+    if(flowerbedSize==1){
+        return 1-flowerbed[0];
+    }if(flowerbed[0]==0&&flowerbed[1]==0){
+        n--;
+        flowerbed[0]=1;
     }
-    return n <= 0;
+    int i=1;
+    while(flowerbed[i-1]==1&&flowerbed[i]==1){
+        i++;
+        if(i>=flowerbedSize) return false;
+    }
+    for(;i<flowerbedSize-1;i++){
+        if(flowerbed[i-1]==0&&flowerbed[i+1]==0&&flowerbed[i]==0){
+            flowerbed[i]=1;
+            n--;
+            i++;
+        }
+    }if(flowerbed[flowerbedSize-1]==0&&flowerbed[flowerbedSize-2]==0) n--;
+    if(n>0) return false;
+    return true;
 }
