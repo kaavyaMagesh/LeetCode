82166d359db713efc07b68a740be0e33fe9a0a5a
kaavyaMagesh <kaavyamagesh1609@gmail.com>
2026-01-28 21:41:30 +0530

    Time: 12 ms (38.13%) | Memory: 16.2 MB (54.52%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,11 +1,18 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key =lambda x:x[0])
-        merged=[]
+class Solution(object):
+    def merge(self, intervals):
+        if not intervals:
+            return []
 
-        for s,e in intervals:
-            if not merged or s > merged[-1][1]:
-             merged.append([s,e])
-            else: #ie s<= merged[-1][1]
-             merged[-1][1]=max(merged[-1][1],e)
-        return merged
+        intervals.sort(key=lambda x: x[0])
+
+        write = 0  # write index
+
+        for i in range(1, len(intervals)):
+            # overlap
+            if intervals[write][1] >= intervals[i][0]:
+                intervals[write][1] = max(intervals[write][1], intervals[i][1])
+            else:
+                write += 1
+                intervals[write] = intervals[i]
+
+        return intervals[:write+1]
