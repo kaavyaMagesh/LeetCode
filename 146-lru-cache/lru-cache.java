69dce8660b51469ffdd446b1885fe5690b2b877d
kaavyaMagesh <kaavyamagesh1609@gmail.com>
2026-08-02 20:44:09 +0530

    Time: 46 ms (56.74%) | Memory: 132.8 MB (19.14%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,90 +1,115 @@
 class LRUCache {
 
+    // Doubly Linked List Node
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
 
-class Node{
-    int key;
-    int value;
-
-    Node next;
-    Node prev;
-
-    Node(int key,int val){
-        this.key=key;
-        this.value=val;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
-}
 
-HashMap<Integer,Node> map;
+    // Maps key -> corresponding node in the linked list
+    HashMap<Integer, Node> map;
 
     int capacity;
-Node head;
-Node tail;
 
+    // Dummy head and tail nodes
+    // head <-> ...actual nodes... <-> tail
+    Node head;
+    Node tail;
 
     public LRUCache(int capacity) {
-        
-        this.capacity=capacity;
 
-        map=new HashMap<>();
+        this.capacity = capacity;
+        map = new HashMap<>();
 
-        head=new Node(-1,-1);
-        tail=new Node(-1,-1);
+        // Create dummy nodes
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
 
-        head.next=tail;
-        tail.prev=head;
-    }
-    
-    private void insert(Node node){
-       tail.prev.next=node;
-       node.prev=tail.prev;
-
-       node.next=tail;
-       tail.prev=node;
+        // Initially head is connected directly to tail
+        head.next = tail;
+        tail.prev = head;
     }
 
-     private void remove(Node node){
- node.prev.next=node.next;
- node.next.prev=node.prev;
+    // Remove a node from the linked list
+    private void remove(Node node) {
+
+        Node previous = node.prev;
+        Node nextNode = node.next;
+
+        previous.next = nextNode;
+        nextNode.prev = previous;
+    }
+
+    // Insert a node just before the tail
+    // This makes it the Most Recently Used (MRU)
+    private void insert(Node node) {
+
+        Node previous = tail.prev;
+
+        previous.next = node;
+        node.prev = previous;
+
+        node.next = tail;
+        tail.prev = node;
     }
 
     public int get(int key) {
-       
-        
-        if(!map.containsKey(key)){
+
+        // Key not found
+        if (!map.containsKey(key))
             return -1;
-        }
 
-         Node node=map.get(key);
+        // Fetch the node
+        Node node = map.get(key);
 
-         remove(node);
-         insert(node);
-         return node.value;
+        // Since it was accessed,
+        // move it to the end (Most Recently Used)
+        remove(node);
+        insert(node);
+
+        return node.value;
     }
-    
+
     public void put(int key, int value) {
-        
-        if(map.containsKey(key)){
-            Node oldnode=map.get(key);
-            remove(oldnode);
+
+        // If key already exists
+        if (map.containsKey(key)) {
+
+            // Remove old node from DLL
+            Node oldNode = map.get(key);
+            remove(oldNode);
+
+            // Remove old mapping
             map.remove(key);
-            
         }
 
-         Node node=new Node(key,value);
-            insert(node);
-            map.put(key,node);
+        // Create new node
+        Node newNode = new Node(key, value);
 
-        if(map.size()> capacity){
-Node lru=head.next;
-remove(lru);
-map.remove(lru.key);
+        // Insert into DLL
+        insert(newNode);
+
+        // Insert into HashMap
+        map.put(key, newNode);
+
+        // Capacity exceeded
+        if (map.size() > capacity) {
+
+            // First real node = Least Recently Used
+            Node lru = head.next;
+
+            // Remove it from DLL
+            remove(lru);
+
+            // Remove it from HashMap
+            map.remove(lru.key);
         }
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
